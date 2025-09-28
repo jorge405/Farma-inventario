1cc436be5670b64f38838d2d5518d3d766575d48
jorge405 <jorgepomsjhope7@gmail.com>
2025-09-27 22:18:23 -0400

    agregandocambios al producto

--- src/views/producto/producto.vue
+++ src/views/producto/producto.vue
@@ -250,86 +250,9 @@ export default{
 <template>
     <AdminLayout>
         <Proveedor></Proveedor>
-        <div class="grid grid-cols-2 gap-6">
-            <ComponentCard title="Registrar Compra">
-                <div class=" justify-end flex "> 
-                        <i class=" pi pi-plus text-white border-2 rounded-full p-2 border-gray-800 cursor-pointer hover:bg-gray-500 hover:transform hover:rotate-180 duration-300"></i>
-                </div>
-                 <div class=" grid grid-cols-3 gap-2 ">
-                        <div>
-                            <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">Fecha vencimiento</label>
-                            <div class="relative">
-                                <flat-pickr
-                                v-model="fecha"
-                                :config="flatpickrConfig"
-                                class="dark:bg-dark-900 h-11 w-full appearance-none rounded-lg border border-gray-700 bg-transparent bg-none px-4 py-2.5 pl-4 pr-11 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-700 focus:border-brand-700 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800"
-                                placeholder="ingrese fecha"/>
-                                <span
-                                class="absolute text-gray-500 -translate-y-1/2 pointer-events-none right-3 top-1/2 dark:text-gray-400">
-                                <svg
-                                class="fill-current"
-                                width="20"
-                                height="20"
-                                viewBox="0 0 20 20"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                fill-rule="evenodd"
-                                clip-rule="evenodd"
-                                d="M6.66659 1.5415C7.0808 1.5415 7.41658 1.87729 7.41658 2.2915V2.99984H12.5833V2.2915C12.5833 1.87729 12.919 1.5415 13.3333 1.5415C13.7475 1.5415 14.0833 1.87729 14.0833 2.2915V2.99984L15.4166 2.99984C16.5212 2.99984 17.4166 3.89527 17.4166 4.99984V7.49984V15.8332C17.4166 16.9377 16.5212 17.8332 15.4166 17.8332H4.58325C3.47868 17.8332 2.58325 16.9377 2.58325 15.8332V7.49984V4.99984C2.58325 3.89527 3.47868 2.99984 4.58325 2.99984L5.91659 2.99984V2.2915C5.91659 1.87729 6.25237 1.5415 6.66659 1.5415ZM6.66659 4.49984H4.58325C4.30711 4.49984 4.08325 4.7237 4.08325 4.99984V6.74984H15.9166V4.99984C15.9166 4.7237 15.6927 4.49984 15.4166 4.49984H13.3333H6.66659ZM15.9166 8.24984H4.08325V15.8332C4.08325 16.1093 4.30711 16.3332 4.58325 16.3332H15.4166C15.6927 16.3332 15.9166 16.1093 15.9166 15.8332V8.24984Z"
-                                fill=""/></svg>
-                                </span>
-                            </div>
-                        </div>
-                        <div> 
-                        <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">Cantidad inicial</label>
-                        <input type="text"  placeholder="ingrese cantidad" class="dark:bg-dark-900 h-11 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800"/>
-                        </div>
-                        <div> 
-                        <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">Precio</label>
-                        <input type="text"  placeholder="ingrese cantidad" class="dark:bg-dark-900 h-11 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800"/>
-                        </div>
-                        <div>
-                            <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">
-                            Seleccione proveedor
-                            </label>
-                            <div class="relative z-20 bg-transparent">
-                                <select
-                                v-model="laboratorio"    
-                                class="dark:bg-dark-900 h-11 w-full appearance-none rounded-lg border border-gray-300 bg-transparent bg-none px-4 py-2.5 pr-11 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-700 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800"
-                                :class="{ 'text-gray-800 dark:text-white/90':laboratorio }"
-                                >
-                                <option value="" selected disabled>Seleciona una opcion</option>
-                                <option v-for="item in proveedor " :key="item.cod_proveedor" :value="item.nombre_proveedor" class="text-gray-700 dark:bg-gray-900 dark:text-gray-400">
-                                    {{ item.nombre_proveedor }}
-                                </option>
-                                </select>
-                                <span
-                                class="absolute z-30 text-gray-700 -translate-y-1/2 pointer-events-none right-4 top-1/2 dark:text-gray-400"
-                                >
-                                <svg
-                                    class="stroke-current"
-                                    width="20"
-                                    height="20"
-                                    viewBox="0 0 20 20"
-                                    fill="none"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                    d="M4.79175 7.396L10.0001 12.6043L15.2084 7.396"
-                                    stroke=""
-                                    stroke-width="1.5"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    />
-                                </svg>
-                                </span>
-                            </div>
-                        </div>
-                        <button type="button" @click="abrirModal"  class=" mt-6 h-11 text-white bg-orange-500 hover:bg-orange-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-orange-500 dark:hover:bg-orange-700 dark:focus:ring-primary-800"><i class=" pi pi-eye mx-1"></i>Detalle</button>   
-                        
-                 </div>   
-                <hr class=" bg-gray-600">
+        <div class="grid grid-cols-1 gap-6">
+            <ComponentCard title="Registrar Compra" v-if="false">
+
                 <h3 class="text-gray-300 text-md font-semibold">Nuevo Producto</h3>
                 <form method="post">
                     <div class=" grid grid-cols-2 xs:grid-cols-1 space-y-6 space-x-2 ">
@@ -499,11 +422,68 @@ export default{
                     <button type="button" @click="addProducto" class="w-full text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Registrar</button>
                     
                 </form>
-            
+                <hr class=" bg-gray-600">
+                <div class=" justify-end flex "> 
+                        <i class=" pi pi-plus text-white border-2 rounded-full p-2 border-gray-800 cursor-pointer hover:bg-gray-500 hover:transform hover:rotate-180 duration-300"></i>
+                </div>
+                 <div class=" grid grid-cols-3 gap-2 ">
+                        <div> 
+                        <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">Cantidad inicial</label>
+                        <input type="text"  placeholder="ingrese cantidad" class="dark:bg-dark-900 h-11 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800"/>
+                        </div>
+                        <div> 
+                        <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">Precio</label>
+                        <input type="text"  placeholder="ingrese cantidad" class="dark:bg-dark-900 h-11 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800"/>
+                        </div>
+                        <div>
+                            <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">
+                            Seleccione proveedor
+                            </label>
+                            <div class="relative z-20 bg-transparent">
+                                <select
+                                v-model="laboratorio"    
+                                class="dark:bg-dark-900 h-11 w-full appearance-none rounded-lg border border-gray-300 bg-transparent bg-none px-4 py-2.5 pr-11 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-700 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800"
+                                :class="{ 'text-gray-800 dark:text-white/90':laboratorio }"
+                                >
+                                <option value="" selected disabled>Seleciona una opcion</option>
+                                <option v-for="item in proveedor " :key="item.cod_proveedor" :value="item.nombre_proveedor" class="text-gray-700 dark:bg-gray-900 dark:text-gray-400">
+                                    {{ item.nombre_proveedor }}
+                                </option>
+                                </select>
+                                <span
+                                class="absolute z-30 text-gray-700 -translate-y-1/2 pointer-events-none right-4 top-1/2 dark:text-gray-400"
+                                >
+                                <svg
+                                    class="stroke-current"
+                                    width="20"
+                                    height="20"
+                                    viewBox="0 0 20 20"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                    d="M4.79175 7.396L10.0001 12.6043L15.2084 7.396"
+                                    stroke=""
+                                    stroke-width="1.5"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    />
+                                </svg>
+                                </span>
+                            </div>
+                        </div>
+                        <button type="button" @click="abrirModal"  class=" mt-6 h-11 text-white bg-orange-500 hover:bg-orange-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-orange-500 dark:hover:bg-orange-700 dark:focus:ring-primary-800"><i class=" pi pi-eye mx-1"></i>Detalle</button>   
+                        
+                 </div>   
         </ComponentCard>
         <ComponentCard title="Lista de Producto">
+            <div class=" flex justify-between">
+                <div class=" justify-start flex">
+                <input type="text" placeholder=" buscar producto" class=" bg-slate-600 min-w-lg rounded-lg p-2 text-sm text-slate-100">
+            </div>
             <div class=" justify-end flex "> 
                 <i class=" pi pi-plus text-white border-2 rounded-full p-2 border-gray-800 cursor-pointer hover:bg-gray-500 hover:transform hover:rotate-180 duration-300"></i>
+            </div>
             </div>
         <div class="overflow-hidden rounded-xl border border-gray-200 bg-white dark:border-gray-800 dark:bg-white/[0.03]">
             <div class="max-w-full overflow-x-auto custom-scrollbar">
